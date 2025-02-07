3f1c8e0490e50c9bfddbe1447592ca1073a84ac0
rafafrassetto <rafafrass@gmail.com>
2025-02-06 23:03:44 -0300

    chore : ngrok configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/index', function () {
+    $path = public_path('../public/index.html');
+    return File::exists($path) ? Response::file($path) : abort(404);
 });
